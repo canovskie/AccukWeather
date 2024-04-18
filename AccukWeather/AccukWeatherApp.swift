0bac1789a26c4f2abc60b1108cca4ceb03304ac9
Can Kaynar <cannkaynarr@outlook.com>
2024-04-18 14:32:50 +0300

    Initial Commit

---
+++ AccukWeather/AccukWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccukWeatherApp.swift
+//  AccukWeather
+//
+//  Created by Can on 18.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct AccukWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
